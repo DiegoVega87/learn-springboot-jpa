ca5290f2c0f5d8884c61ceb59526f9b9e33f38e4
DiegoVega87 <diegovega@me.com>
2024-02-13 19:19:25 +0100

    First commit

---
+++ src/main/java/com/learnjpaexample/jpalearningpractice/JpaLearningPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.learnjpaexample.jpalearningpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLearningPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLearningPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnjpaexample/jpalearningpractice/JpaLearningPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnjpaexample.jpalearningpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLearningPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
